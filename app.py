de7c55b627713f6cf4569d25e4db6a37b268102f
besttimepp-pixel <besttimepp@gmail.com>
2026-05-26 18:36:36 +0700

    fix reply

--- app.py
+++ app.py
@@ -7,30 +7,26 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 
 load_dotenv()
-
 app = Flask(__name__)
-
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
-
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers.get("X-Line-Signature")
     body = request.get_data(as_text=True)
-
     try:
         handler.handle(body, signature)
     except Exception:
         return "OK", 200
+    return "OK", 200
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="สวัสดีครับ")
-    )
+)
     return "OK", 200
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
