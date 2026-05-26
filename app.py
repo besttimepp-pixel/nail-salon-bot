e731c2f650b9093d3c4f424301f18f232ecb7f6f
besttimepp-pixel <besttimepp@gmail.com>
2026-05-26 17:42:11 +0700

    final fix

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ def callback():
         return "OK", 200
 
     return "OK", 200
-
-
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
